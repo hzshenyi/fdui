c2abb676a7953ddf938608e515d8edcc487611bc
shenyi <68382054@qq.com>
2021-03-21 17:07:07 +0800

    t

--- src/webide/view/components/DomTree.vue
+++ src/webide/view/components/DomTree.vue
@@ -7,7 +7,10 @@
     <!-- 非文本 -->
     <template v-if="el.tagName!='text'">
       <span class="fd-tag" @click="selectDom(el)"  @mouseover.stop="el.$$overed = true"  @mouseout="el.$$overed = false">&lt;{{el.tagName}}></span>
-        <span  class="fd-innerHtml" v-if="el.innerHTML" @dblclick="modifyInnerHtml(el,$event)">{{ !el.$$edited?el.innerHTML:"" }}</span>     
+        <span  class="fd-innerHtml" v-if="el.innerHTML" @dblclick="modifyInnerHtml(el,$event)">
+          <!-- 阅读模式 -->
+          <span v-if="!el.$$edited"  class="fd-innerHtml-read">{{ el.innerHTML }}</span>
+          </span>     
     
       <DomTree v-if="el.children" :elList="el.children"></DomTree>
       <span  class="fd-tag"  @click="selectDom(el)"  @mouseover.stop="el.$$overed = true"  @mouseout="el.$$overed = false">&lt;/{{el.tagName}}></span>
@@ -15,7 +18,10 @@
     <!-- 如果是文本 -->
       <template v-if="el.tagName=='text'">
       <span   @mouseover.stop="el.$$overed = true"  @mouseout="el.$$overed = false" class="fd-innerHtml" v-if="el.innerHTML"
-       @dblclick="modifyInnerHtml(el,$event)">{{ !el.$$edited?el.innerHTML:"" }}</span>     
+       @dblclick="modifyInnerHtml(el,$event)">
+       <!-- 阅读模式 -->
+       <span v-if="!el.$$edited"  class="fd-innerHtml-read">{{ el.innerHTML }}</span>
+       </span>     
     </template>
       </li>
   </ul>
@@ -48,11 +54,22 @@ const modifyInnerHtml = (el,dom)=>{
  const h = spanObj.offsetHeight;
  const computedStyle = document.defaultView.getComputedStyle(spanObj, null); 
  const c = computedStyle.backgroundColor;
- const textareaObj = document.createElement("textarea");
- textareaObj.innerHTML = el.innerHTML;
+ //如果没有编辑字段
+ if(spanObj.children.length==1){
+  const textareaObj = document.createElement("textarea");
+  textareaObj.innerHTML = el.innerHTML;
+  // dom.currentTarget.innerHTML = `<textarea style="margin:0;padding:0;border:0;width:${w}px;height:${h}px;background-color:${c}">${el.innerHTML}</textarea>`
+  textareaObj.style = `margin:0;padding:0;border:0;width:${w}px;height:${h}px;background-color:${c}`
+  textareaObj.onchange = (e)=>{
+      el.innerHTML = e.currentTarget.value;
+      el.$$edited = false;
+      e.currentTarget.style.display = "none";
+  }
+  spanObj.appendChild(textareaObj)
+ }else{
+   spanObj.children[1].style.display = "";//显示编辑字段
+ }
  
- // dom.currentTarget.innerHTML = `<textarea style="margin:0;padding:0;border:0;width:${w}px;height:${h}px;background-color:${c}">${el.innerHTML}</textarea>`
  el.$$edited = !el.$$edited;
- spanObj.appendChild(textareaObj)
 }
 </script>
